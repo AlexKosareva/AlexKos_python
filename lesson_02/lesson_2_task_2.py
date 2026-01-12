f3e2cda5fd85fcab2cd723ce56c7f0f22fe75e1c
AlexKosareva <izverg.89@gmail.com>
2026-01-12 17:06:26 +0300

    Только 4 урок

--- lesson_02/lesson_2_task_2.py
+++ lesson_02/lesson_2_task_2.py
@@ -1,7 +1,8 @@
+
 def is_year_leap(year):
     return year % 4 == 0
 user_input_str = input("Введите год для проверки: ")
-2
+
 try:
     year_value = int(user_input_str)
     is_leap = is_year_leap(year_value)
